6681466a1c3eefced60d8c2e1b54d59a75ef2cc5
Eiffel Chong <eiffelchongsy@gmail.com>
2024-09-10 14:21:51 +0800

    Fixed auth toasts on mobile

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,82 +1,137 @@
 <template>
   <div
-    class="min-h-screen bg-zinc-100 dark:bg-zinc-900 flex items-center justify-center p-4 md:p-6 lg:p-8"
+    class="min-h-screen bg-zinc-100 dark:bg-zinc-900 flex flex-col lg:flex-row items-center justify-between"
   >
-  <div class="w-full md:flex md:flex-row">
-    <Card class="w-full bg-white dark:bg-zinc-800">
-      <CardHeader>
-        <CardTitle
-          class="text-2xl md:text-3xl lg:text-4xl font-bold text-center text-zinc-900 dark:text-white"
-        >
-          {{ authType === "login" ? "Login" : "Register" }}
-        </CardTitle>
-        <CardDescription class="text-sm sm:text-base text-center text-zinc-600 dark:text-zinc-300">
-          {{ authType === "login" ? "Welcome back!" : "Join us today!" }}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form @submit.prevent="handleSubmit" class="space-y-4 sm:space-y-6">
-          <div class="space-y-2">
-            <Label for="email" class="text-sm sm:text-base text-zinc-900 dark:text-zinc-300">Email</Label>
-            <Input
-              id="email"
-              v-model="email"
-              type="email"
-              placeholder="example@mail.com"
-              required
-              class="w-full text-sm sm:text-base p-2 sm:p-3 bg-zinc-50 dark:bg-zinc-700 text-zinc-900 dark:text-zinc-200 border dark:border-zinc-600"
-              autocomplete="email"
+    <div class="hidden lg:block lg:w-3/5">
+      <Carousel
+        class="w-full"
+        :plugins="[
+          Autoplay({
+            delay: 3800,
+          }),
+        ]"
+        @select="updateSlide"
+        :slideIndex="currentSlide"
+      >
+        <CarouselContent class="-ml-0">
+          <CarouselItem class="pl-0">
+            <img
+              class="w-full h-screen dark:brightness-90"
+              src="/public/auth-stock-1.webp"
             />
-          </div>
-          <div class="space-y-2">
-            <Label for="password" class="text-sm sm:text-base text-zinc-900 dark:text-zinc-300">Password</Label>
-            <Input
-              id="password"
-              v-model="password"
-              type="password"
-              placeholder="Password"
-              required
-              class="w-full text-sm sm:text-base p-2 sm:p-3 bg-zinc-50 dark:bg-zinc-700 text-zinc-900 dark:text-zinc-200 border dark:border-zinc-600"
-              autocomplete="new-password"
-            />
-          </div>
-          <div v-if="authType === 'register'" class="space-y-2">
-            <Label for="confirmPassword" class="text-sm sm:text-base text-zinc-900 dark:text-zinc-300"
-              >Confirm Password</Label
-            >
-            <Input
-              id="confirmPassword"
-              v-model="confirmPassword"
-              type="password"
-              placeholder="Re-enter Password"
-              required
-              class="w-full text-sm sm:text-base p-2 sm:p-3 bg-zinc-50 dark:bg-zinc-700 text-zinc-900 dark:text-zinc-200 border dark:border-zinc-600"
-              autocomplete="new-password"
-            />
-          </div>
-          <Button
-            type="submit"
-            class="w-full text-sm sm:text-base py-2 sm:py-3 bg-zinc-500 dark:bg-zinc-600 text-white"
+          </CarouselItem>
+          <CarouselItem class="pl-0"><img
+              class="w-full h-screen dark:brightness-90"
+              src="/public/auth-stock-2.webp"
+            /></CarouselItem>
+          <CarouselItem class="pl-0"><img
+              class="w-full h-screen dark:brightness-90"
+              src="/public/auth-stock-3.webp"
+            /></CarouselItem>
+        </CarouselContent>
+        <div class="absolute bottom-4 w-full flex justify-center space-x-2">
+          <span
+            v-for="(slide, index) in slides"
+            :key="index"
+            :class="[
+              'w-3 h-3 rounded-full cursor-pointer transition-all duration-300',
+              currentSlide === index ? 'bg-zinc-500 dark:bg-zinc-300' : 'bg-zinc-300 dark:bg-zinc-600'
+            ]"
+            @click="goToSlide(index)"
+          ></span>
+        </div>
+      </Carousel>
+    </div>
+    <div class="w-full h-screen lg:w-2/5 p-8 flex lg:justify-center">
+      <Card class="w-full self-center lg:order-last bg-white dark:bg-zinc-800">
+        <CardHeader>
+          <CardTitle
+            class="text-2xl md:text-3xl lg:text-4xl font-bold text-center text-zinc-900 dark:text-white"
           >
-            {{ authType === "login" ? "Sign In" : "Sign Up" }}
+            {{ authType === "login" ? "Login" : "Register" }}
+          </CardTitle>
+          <CardDescription
+            class="text-sm sm:text-base text-center text-zinc-600 dark:text-zinc-300"
+          >
+            {{ authType === "login" ? "Welcome back!" : "Join us today!" }}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form @submit.prevent="handleSubmit" class="space-y-4 sm:space-y-6">
+            <div class="space-y-2">
+              <Label
+                for="email"
+                class="text-sm sm:text-base text-zinc-900 dark:text-zinc-300"
+                >Email</Label
+              >
+              <Input
+                id="email"
+                v-model="email"
+                type="email"
+                placeholder="example@mail.com"
+                required
+                class="w-full text-sm sm:text-base p-2 sm:p-3 bg-zinc-50 dark:bg-zinc-700 text-zinc-900 dark:text-zinc-200 border dark:border-zinc-600"
+                autocomplete="email"
+              />
+            </div>
+            <div class="space-y-2">
+              <Label
+                for="password"
+                class="text-sm sm:text-base text-zinc-900 dark:text-zinc-300"
+                >Password</Label
+              >
+              <Input
+                id="password"
+                v-model="password"
+                type="password"
+                placeholder="Password"
+                required
+                class="w-full text-sm sm:text-base p-2 sm:p-3 bg-zinc-50 dark:bg-zinc-700 text-zinc-900 dark:text-zinc-200 border dark:border-zinc-600"
+                autocomplete="new-password"
+              />
+            </div>
+            <div v-if="authType === 'register'" class="space-y-2">
+              <Label
+                for="confirmPassword"
+                class="text-sm sm:text-base text-zinc-900 dark:text-zinc-300"
+                >Confirm Password</Label
+              >
+              <Input
+                id="confirmPassword"
+                v-model="confirmPassword"
+                type="password"
+                placeholder="Re-enter Password"
+                required
+                class="w-full text-sm sm:text-base p-2 sm:p-3 bg-zinc-50 dark:bg-zinc-700 text-zinc-900 dark:text-zinc-200 border dark:border-zinc-600"
+                autocomplete="new-password"
+              />
+            </div>
+            <Button
+              type="submit"
+              class="w-full text-sm sm:text-base py-2 sm:py-3 bg-zinc-500 dark:bg-zinc-600 text-white"
+            >
+              {{ authType === "login" ? "Sign In" : "Sign Up" }}
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter class="flex justify-center">
+          <Button
+            variant="link"
+            @click="toggleAuthType"
+            class="text-xs sm:text-sm md:text-base text-zinc-500 dark:text-zinc-400"
+          >
+            {{
+              authType === "login"
+                ? "Don't have an account? Sign Up"
+                : "Already have an account? Sign In"
+            }}
           </Button>
-        </form>
-      </CardContent>
-      <CardFooter class="flex justify-center">
-        <Button
-          variant="link"
-          @click="toggleAuthType"
-          class="text-xs sm:text-sm md:text-base text-zinc-500 dark:text-zinc-400"
-        >
-          {{
-            authType === "login"
-              ? "Don't have an account? Sign Up"
-              : "Already have an account? Sign In"
-          }}
-        </Button>
-      </CardFooter>
-    </Card>
-      <Button @click="toggleDarkMode" class="absolute top-2 right-2 p-2 size-9 bg-zinc-200 dark:bg-zinc-800 text-black dark:text-white rounded-full">
+        </CardFooter>
+      </Card>
+      <Button
+        @click="toggleDarkMode"
+        class="absolute top-2 right-2 p-2 size-9 bg-zinc-200 dark:bg-zinc-800 text-black dark:text-white rounded-full"
+      >
         <SunIcon v-if="isDark" />
         <MoonIcon v-else />
       </Button>
@@ -88,8 +143,14 @@
 <script setup>
 import { h, ref } from "vue";
 import { SunIcon, MoonIcon } from "@radix-icons/vue";
-import { useToast } from '@/components/ui/toast/use-toast';
-import { Toaster, ToastAction } from '@/components/ui/toast';
+import { useToast } from "@/components/ui/toast/use-toast";
+import { Toaster, ToastAction } from "@/components/ui/toast";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 const { $supabase } = useNuxtApp();
 const { toast } = useToast();
@@ -100,6 +161,21 @@ const confirmPassword = ref("");
 const authType = ref("login");
 const passwordMismatch = ref(false);
 const isDark = ref(false);
+const currentSlide = ref(0);
+
+const slides = ref([
+  { src: "/public/auth-stock.webp" },
+  { src: "/public/another-slide.webp" },
+  { src: "/public/third-slide.webp" },
+]);
+
+const updateSlide = (index) => {
+  currentSlide.value = index;
+};
+
+const goToSlide = (index) => {
+  currentSlide.value = index;
+};
 
 const toggleAuthType = () => {
   authType.value = authType.value === "login" ? "register" : "login";
@@ -116,11 +192,15 @@ const handleSubmit = async () => {
         title: "Login Failed",
         description: error.message,
         variant: "destructive",
-        action: h(ToastAction, {
-          altText: 'Try again',
-        }, {
-          default: () => 'Try again',
-        }),
+        action: h(
+          ToastAction,
+          {
+            altText: "Try again",
+          },
+          {
+            default: () => "Try again",
+          }
+        ),
       });
     } else {
       toast({
@@ -140,11 +220,15 @@ const handleSubmit = async () => {
         title: "Uh oh! Something went wrong.",
         description: "Passwords do not match.",
         variant: "destructive",
-        action: h(ToastAction, {
-          altText: 'Try again',
-        }, {
-          default: () => 'Try again',
-        }),
+        action: h(
+          ToastAction,
+          {
+            altText: "Try again",
+          },
+          {
+            default: () => "Try again",
+          }
+        ),
       });
       return;
     }
@@ -158,11 +242,15 @@ const handleSubmit = async () => {
         title: "Registration Failed",
         description: error.message,
         variant: "destructive",
-        action: h(ToastAction, {
-          altText: 'Try again',
-        }, {
-          default: () => 'Try again',
-        }),
+        action: h(
+          ToastAction,
+          {
+            altText: "Try again",
+          },
+          {
+            default: () => "Try again",
+          }
+        ),
       });
     } else {
       toast({
@@ -177,9 +265,9 @@ const handleSubmit = async () => {
 const toggleDarkMode = () => {
   isDark.value = !isDark.value;
   if (isDark.value) {
-    document.documentElement.classList.add('dark');
+    document.documentElement.classList.add("dark");
   } else {
-    document.documentElement.classList.remove('dark');
+    document.documentElement.classList.remove("dark");
   }
 };
 </script>
