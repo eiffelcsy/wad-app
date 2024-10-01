1ee417731de491f4fe38eb40ca56ab711bbadac8
Eiffel Chong <eiffelchongsy@gmail.com>
2024-10-01 12:57:31 +0800

    Fixed viewport zooming on mobile part 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,6 @@ export default defineNuxtConfig({
         viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
       },
     },
-  },
   imports: {
     autoImport: true,
   },
