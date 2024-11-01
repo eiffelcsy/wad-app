a8df13de15014139989e9b5f44a283fdbd528270
zhengjieee <zj.toh.2023@scis.smu.edu.sg>
2024-11-01 21:33:08 +0800

    Rough skeleton of individual team page

--- pages/teams.vue
+++ pages/teams.vue
@@ -236,7 +236,7 @@ const fetchTeams = async () => {
 
 const formatDate = (dateString) => {
   const options = { year: "numeric", month: "2-digit", day: "2-digit" };
-  return new Date(dateString).toLocaleDateString(undefined, options);
+  return new Date(dateString).toLocaleDateString("en-GB", options);
 };
 
 const filteredTeams = computed(() => {
