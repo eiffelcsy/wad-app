540428e4c7f46e89779983b632289d2e50576ce6
Eiffel Chong <eiffelchongsy@gmail.com>
2024-11-11 23:21:01 +0800

    Added lines to central point

--- components/custom/google-maps/GoogleMaps.vue
+++ components/custom/google-maps/GoogleMaps.vue
@@ -16,7 +16,13 @@
       <Polyline
         v-for="(location, index) in userLocations"
         :key="'polyline-' + index"
-        :path="[location, centralPoint]"
+        :options="{
+            path: [location, centralPoint],
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2,
+        }"
       />
     </GoogleMap>
   </div>
