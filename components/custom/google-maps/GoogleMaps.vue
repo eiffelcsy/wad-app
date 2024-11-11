34b6e26564e0239596646d8096d23e0382e9dc55
Eiffel Chong <eiffelchongsy@gmail.com>
2024-11-11 23:11:16 +0800

    Added debug to mobile browsers

--- components/custom/google-maps/GoogleMaps.vue
+++ components/custom/google-maps/GoogleMaps.vue
@@ -59,7 +59,7 @@ const getUserLocation = () => {
       },
       (error) => {
         console.error(error);
-        alert("Unable to retrieve your location.", error);
+        alert(`Unable to retrieve your location. ${error.message}`);
       }
     );
   } else {
